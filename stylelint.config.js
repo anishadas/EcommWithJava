3034e140eb77c1c00c5281eef7481d413c7dc357
Anisha <payalad10@gmail.com>
2025-02-13 21:15:09 +0530

    stylelint fix

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": [
+        "stylelint-config-tailwindcss"
+    ],
+    "rules": {
+        "at-rule-no-unknown": [
+            true,
+            {
+                "ignoreAtRules": ["tailwind", "apply", "variants", "responsive", "screen"]
+            }
+        ]
+    }
+}
