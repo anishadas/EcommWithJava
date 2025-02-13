72638d53a9e4bcb35e83d459629d6a04184483a6
Anisha <payalad10@gmail.com>
2025-02-13 20:09:22 +0530

    Ecomm initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navigation from "./customer/components/Navigation/Navigation";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      home
+    </div>
+  );
+}
+
+export default App;
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCarousel />
+            HomePage
+        </div>
+    )
+}
+
+export default HomePage
